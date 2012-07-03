a544b5a8291adf5721b6c1e9cbe9a8b1334b2844
Andrew Serputko <aserputko@gmail.com>
2012-07-03 11:00:41 +0400

    Updated seed file

--- db/seeds.rb
+++ db/seeds.rb
@@ -1,7 +1,14 @@
-# This file should contain all the record creation needed to seed the database with its default values.
-# The data can then be loaded with the rake db:seed (or created alongside the db with db:setup).
-#
-# Examples:
-#
-#   cities = City.create([{ name: 'Chicago' }, { name: 'Copenhagen' }])
-#   Mayor.create(name: 'Emanuel', city: cities.first)
+Project.create!(name: "Complete the test task for Ruby Garage")
+
+Task.create!(name: "I want to be able to create/update/delete projects ", status: true, project_id: 1)
+Task.create!(name: "I want to be able to add tasks to my project", status: true, project_id: 1)
+Task.create!(name: "I want to be able to update/delete tasks", status: true, project_id: 1)
+Task.create!(name: "I want to be able to prioritise tasks into a project", status: true, project_id: 1)
+Task.create!(name: "I want to be able to choose deadline for my task", status: false, project_id: 1)
+Task.create!(name: "I want to be able to mark a task as 'done'", status: true, project_id: 1)
+Task.create!(name: "It should be WEB application: For the client  HTML, CSS (Twitter Bootstrap), JavaScript (jQuery, Backbone); For server side : Ruby", status: true, project_id: 1)
+Task.create!(name: "It should have client side and server side validation", status: false, project_id: 1)
+Task.create!(name: "It should look like on screens (see attached file rg_test_task_grid.png)", status: true, project_id: 1)
+Task.create!(name: "It should work like one page WEB application and should use AJAX technology, load and submit data without reloading a page.", status: true, project_id: 1)
+Task.create!(name: "It should have user authentication solution and a user should only have access to their own projects and tasks.", status: false, project_id: 1)
+Task.create!(name: "It should have automated tests for all functionality", status: false, project_id: 1)
